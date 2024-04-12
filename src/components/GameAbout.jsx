c10f2941b5ecf21a2fd63332c21994eaeca219ec
Anders <techboxnorth@gmail.com>
2024-04-12 19:44:18 +0200

    created about component

--- src/components/GameAbout.jsx
+++ src/components/GameAbout.jsx
@@ -8,10 +8,15 @@ function GameAbout(){
                     <h1>ABOUT THE GAME</h1>
                     <p>
                     Shovel Knight is a sweeping, classic action-adventure game with awesome gameplay, memorable characters, and an 8-bit retro aesthetic. It’s a hot mashup of new and old!
-
-You play as the eponymous Shovel Knight, a small warrior with a huge quest. Shovel Knight has come to this land with two goals: to defeat the evil Enchantress and quest for his lost beloved. He wields a Shovel Blade: a multipurpose weapon whose techniques have now been lost to the ages. Always honest and helpful, Shovel Knight is a shining example of the code of Shovelry: Slash Mercilessly and Dig Tirelessly!
+                    </p>
+                    <p>
+                    You play as the eponymous Shovel Knight, a small warrior with a huge quest. Shovel Knight has come to this land with two goals: to defeat the evil Enchantress and quest for his lost beloved. He wields a Shovel Blade: a multipurpose weapon whose techniques have now been lost to the ages. Always honest and helpful, Shovel Knight is a shining example of the code of Shovelry: Slash Mercilessly and Dig Tirelessly!
+                    </p>
+                    <p>
+                    But, between Shovel Knight and his beloved stands a cadre of villainous knights. These terrible foes, known as The Order of No Quarter, have been dispatched to prevent Shovel Knight from reaching the Enchantress, and will pursue their mission at any cost. If you love games with perfect platforming, beautiful art, infectious music, lovable bosses, humor and levity, and real heart… Shovel Knight is for you!
                     </p>
                 </article>
+                <img src="src/assets/screen_06.webp" alt="" />
             </section>
         </>
     );
